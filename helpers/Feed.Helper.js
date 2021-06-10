c576c346686e9d72e13cfd1e5f336d453290fd3d
hermanu <manuguionbajo@gmail.com>
2021-06-10 14:34:50 +0200

    Moved all server files to server folder Fixed start and dev scripts Fixed index.html title

--- helpers/Feed.Helper.js
+++ helpers/Feed.Helper.js
@@ -1,17 +1,17 @@
-const Parser = require("rss-parser");
-const parser = new Parser();
+// const Parser = require("rss-parser");
+// const parser = new Parser();
 
-const validRSSFeedUrls = {
-  redditNews: "https://www.reddit.com/r/news/.rss",
-  twentyMinutes: "https://www.20minutos.es/rss/",
-};
-const getRssFeed = async () => {
-  const { items: redditNews } = await parser.parseURL(
-    validRSSFeedUrls.twentyMinutes
-  );
-  return redditNews;
-};
+// const validRSSFeedUrls = {
+//   redditNews: "https://www.reddit.com/r/news/.rss",
+//   twentyMinutes: "https://www.20minutos.es/rss/",
+// };
+// const getRssFeed = async () => {
+//   const { items: redditNews } = await parser.parseURL(
+//     validRSSFeedUrls.twentyMinutes
+//   );
+//   return redditNews;
+// };
 
-module.exports = {
-  getRssFeed,
-};
+// module.exports = {
+//   getRssFeed,
+// };
